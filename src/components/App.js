c7e4e27b9351079a9ba24d1a800682eca638db0c
emmanuel-mireku <emmanuelmireku15@gmail.com>
2021-01-23 16:51:06 -0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Let's build a covid tracker! Let's GOOOOOOOOOOOOOO!!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
